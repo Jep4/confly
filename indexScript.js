a51cebcd6eb3365b4187255c7db79193d60393d7
Jooeun Park <89099318+Jep4@users.noreply.github.com>
2022-10-09 23:43:51 -0700

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -2,12 +2,16 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser")
 
 const home = require('./src/routes/home/indexRoute.js');
 
 app.set("views", "./src/views");
 app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname+'/src'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/", home);
 
 module.exports = app;
