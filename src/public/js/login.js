61329a3e291500fd78c81cff5e684d6ca9259fb6
Jooeun Park <89099318+Jep4@users.noreply.github.com>
2022-10-09 23:44:36 -0700

    Update login.js

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -2,9 +2,9 @@
 
 "use strict";
 
-const id = document.querySelector("#idpart");
-const pw = document.querySelector("#passwordpart")
-const loginBtn = document.querySelector("#loginB")
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw")
+const loginBtn = document.querySelector("#loginBtn")
 
 loginBtn.addEventListener("click", login);
 
@@ -13,4 +13,20 @@ function login() {
         id: id.value,
         pw: pw.value,
     };
+
+    console.log(req);
+    fetch('/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    }).then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/";
+            } else {
+                alert(res.msg);
+            }
+            });
 }
