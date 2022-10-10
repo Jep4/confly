ad52c8ed732faff402ba9312e2304df4eb6a43a5
Jooeun Park <89099318+Jep4@users.noreply.github.com>
2022-10-09 23:44:10 -0700

    Update homeCtr.js

--- src/routes/home/homeCtr.js
+++ src/routes/home/homeCtr.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const users = ["som", "jun"];
+const pw =["1234", "1234"];
+
+
 function callindex(req, res) {
     res.render("./home/index")
 };
@@ -12,8 +16,28 @@ function profile(req, res) {
     res.render("./home/profile")
 };
 
+function process(req, res) {
+    const id = req.id,
+        pw = req.pw;
+
+    if (users.id.include(id)) {
+        const idx = users.id.indexOf(id);
+        if (users.pw[idx] === pw) {
+            return res.json({
+                success: true,
+            })
+        }
+    }
+
+    return res.json({
+        success: false,
+        msg: "login failed",
+    })
+};
+
 module.exports = {
     callindex,
     login,
     profile,
+    process,
 };
