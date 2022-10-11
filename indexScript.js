8b1f4b337ec33a782eea65fb483560ad21c19922
jep4 <civbf1@gmail.com>
2022-10-10 20:24:16 -0700

    add sql

--- indexScript.js
+++ indexScript.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+var mysqlRouter = require('./src/routes/home/mysql.js');
 
 
 const home = require('./src/routes/home/indexRoute.js');
@@ -13,5 +14,6 @@ app.use(express.static(__dirname+'/src'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", home);
+app.use('/mysql', mysqlRouter);
 
 module.exports = app;
